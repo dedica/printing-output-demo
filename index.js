73517efc9920f90bd4d850f3c50548ac1cbd164e
Amel Dedic <dedica@algonquincollege.com>
2022-01-18 09:07:56 -0500

    JavaScript file added

---
+++ index.js
@@ -0,0 +1,16 @@
+let x = 15;
+let y = 5;
+
+/*
+// concatinating using operator +
+console.log('x = 15, y = 5');
+console.log('x + y = ' + (x + y)); // 20
+console.log('x - y = ' + (x - y)); // 10
+console.log('x * y = ' + (x * y)); // 75
+console.log('x / y = ' + (x / y)); // 3
+console.log('x % y = ' + (x % y)); // 0
+*/
+
+// interpolating variables into string
+console.log(`x = 15, y = 5`);
+console.log(`x + y = ${(x + y)}`); // 20
